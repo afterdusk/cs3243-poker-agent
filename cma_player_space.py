8c82245a9f27f4087bce45ac777a9081aced1585
LeeYiyuan <leeyiyuan@hotmail.com>
2019-03-17 18:54:40 +0800

    Add name. Add state persistence

--- cma_player_space.py
+++ cma_player_space.py
@@ -2,9 +2,13 @@ import cma
 import random
 import itertools
 import math
+import numpy
+import pickle
+import os
 
 class CMAPlayerSpace:
-    def __init__(self, taskmaster, num_dimensions, num_instances, initial_sd, num_games, num_rounds):
+    def __init__(self, taskmaster, name, num_dimensions, num_instances, initial_sd, num_games, num_rounds):
+        self.name = name
         self.taskmaster = taskmaster
         self.num_dimensions = num_dimensions
         self.num_instances = num_instances
@@ -13,27 +17,36 @@ class CMAPlayerSpace:
         self.num_rounds = num_rounds
         self.generation = 0
 
-        self.instances = []        
-        cma_options = cma.CMAOptions()
-        cma_options.set('verbose', -9)
-        cma_options.set('verb_disp', -1)
-        cma_options.set('verb_log', 0)
-        for mean in [[random.uniform(-1, 1) for _ in xrange(num_dimensions)] for _ in xrange(num_instances)]:
-            self.instances += [cma.CMAEvolutionStrategy(mean, initial_sd, cma_options)]
+        if os.path.isfile('cma_state_' + self.name + '.txt'):
+            print('Loading state from file...')
+            with open('cma_state_' + self.name + '.txt', 'rb') as state_file:
+                self.instances = pickle.load(state_file)
+        else:
+            print('Generating new state...')
+            self.instances = []        
+            cma_options = cma.CMAOptions()
+            cma_options.set('verbose', -9)
+            cma_options.set('verb_disp', -1)
+            cma_options.set('verb_log', 0)
+            for mean in [[random.uniform(-1, 1) for _ in xrange(num_dimensions)] for _ in xrange(num_instances)]:
+                self.instances += [cma.CMAEvolutionStrategy(mean, initial_sd, cma_options)]
 
-        with open('cmaes_trainer_log.txt', 'w') as _:
+        with open('cma_log_' + self.name + '.txt', 'w') as _:
             None
 
         self.begin()
 
     def begin(self):
         print('Logging...')
-        with open('cmaes_trainer_log.txt', 'a') as log_file:
+        with open('cma_log_' + self.name + '.txt', 'a') as log_file:
             log_file.write('Generation = ' + str(self.generation) + '\n')
             for (i, instance) in enumerate(self.instances):
                 log_file.write('(' + str(i).zfill(2) + ') Mean = ' + ' '.join(str(x) for x in instance.result[5]) + '\n')
                 log_file.write('(' + str(i).zfill(2) + ') SD =   ' + ' '.join(str(x) for x in instance.result[6]) + '\n')
+                log_file.write('(' + str(i).zfill(2) + ') Norm(SD) = ' + str(numpy.linalg.norm(instance.result[6], ord=2)) + '\n')
             log_file.write('\n')
+        with open('cma_state_' + self.name + '.txt', 'wb') as state_file:
+            pickle.dump(self.instances, state_file)
 
         print('Generating jobs...')
 
