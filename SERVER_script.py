afb651becd78ff6fcc223276bc6dcf3451c95095
LeeYiyuan <leeyiyuan@hotmail.com>
2019-03-23 20:09:35 +0800

    Add SmartWarrior training

--- SERVER_script.py
+++ SERVER_script.py
@@ -10,30 +10,42 @@ from profile_player_space import ProfilePlayerSpace
 
 def createPlayerSpaces(taskmaster):
     # Init player spaces here
-    # david_playerspace.init(taskmaster)
+    david_playerspace.init(taskmaster)
     # botlympics.init(taskmaster)
     return [
-        ProfilePlayerSpace(
+        CMAPlayerSpace(
             taskmaster,
-            'smart_warrior_profile_1',
+            'smart_warrior_train_1',
             'SmartWarrior',
-            [[0, 1]] * 50,
-            20,
-            2000,
+            [[0, 1]] * 50, # weight ranges
+            0.3, # initial sd
+            2500, # samples per evaluation
             4,
             101,
             60 * 2)
+        
+        #ProfilePlayerSpace(
+        #    taskmaster,
+        #    'smart_warrior_profile_1',
+        #    'SmartWarrior',
+        #    [[0, 1]] * 50,
+        #    20,
+        #    2000,
+        #    4,
+        #    101,
+        #    60 * 2)
 
         #CMAPlayerSpace(
         #    taskmaster,
         #    'neural_player_test_4',
         #    'NeuralPlayer',
         #    [[0, 1]] * 50, # weight ranges
-        #    0.15, # initial sd
+        #    0.3, # initial sd
         #    2500, # samples per evaluation
         #    4,
         #    101,
         #    60 * 2)
+        
         #ProfilePlayerSpace(
         #    taskmaster,
         #    'neural_player_profile_1',
