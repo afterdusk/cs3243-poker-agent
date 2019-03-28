770dcced9129ed2821e806192a2ff193e0351768
E-Liang Tan <eliang@eliangtan.com>
2019-03-28 18:04:53 +0800

    Replace .gitignore with version from gitignore.io

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(
+        [
+            Extension("*", ["*.py"]),
+            Extension("syncengine.*", ["syncengine/*.py"]),
+            Extension("pypokerengine.*", ["pypokerengine/*.py"]),
+            Extension("pypokerengine.api.*", ["pypokerengine/api/*.py"]),
+            Extension("pypokerengine.engine.*", ["pypokerengine/engine/*.py"]),
+            Extension("pypokerengine.utils.*", ["pypokerengine/utils/*.py"]),
+        ],
+        exclude=["Group23Player.py"],
+        build_dir="build",
+        compiler_directives=dict(
+            always_allow_keywords=True
+        )
+    )
+)
+
