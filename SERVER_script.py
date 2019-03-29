32f799923e8a0d1c5600e044f547d6a3ed535edf
LeeYiyuan <leeyiyuan@hotmail.com>
2019-03-29 18:03:48 +0800

    Add CMAPlayerSpace to train EpsilonPlayer directly

--- SERVER_script.py
+++ SERVER_script.py
@@ -10,32 +10,42 @@ from profile_player_space import ProfilePlayerSpace
 
 def createPlayerSpaces(taskmaster):
     # Init player spaces here
-    botlympics.init(taskmaster)
+    #botlympics.init(taskmaster)
 
     david_playerspace.init(taskmaster, "2903_ChampPlat_Board")
 
     CMAPlayerSpace(
-        taskmaster,
-        'neural3_player_train_cherry',
-        'Neural3Player',
-        [[-1, 1]] * 10 + [[0, 1]] * 2,
-        4000,
-        4,
-        101,
-        60 * 4)
+            taskmaster,
+            'epsilon_player_train_apple',
+            'EpsilonPlayer',
+            [[-1, 1]] * 12,
+            4000,
+            4,
+            101,
+            60 * 4)
 
-    ProfilePlayerSpace(
-        taskmaster,
-        'neural3_player_profile_cherry',
-        'Neural3Player',
-        10,
-        [[-1, 1]] * 10 + [[0, 1]] * 2,
-        20,
-        4000,
-        1000,
-        4,
-        101,
-        60 * 4)
+    #CMAPlayerSpace(
+    #    taskmaster,
+    #    'neural3_player_train_cherry',
+    #    'Neural3Player',
+    #    [[-1, 1]] * 10 + [[0, 1]] * 2,
+    #    4000,
+    #    4,
+    #    101,
+    #    60 * 4)
+
+    #ProfilePlayerSpace(
+    #    taskmaster,
+    #    'neural3_player_profile_cherry',
+    #    'Neural3Player',
+    #    10,
+    #    [[-1, 1]] * 10 + [[0, 1]] * 2,
+    #    20,
+    #    4000,
+    #    1000,
+    #    4,
+    #    101,
+    #    60 * 4)
 
 class TrainerServer:
     def __init__(self, client_id=None):
