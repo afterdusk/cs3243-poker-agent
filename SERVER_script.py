789e6fe8aff9bf0aa3864ecf006dccf90fb7564e
Lee Yiyuan <leeyiyuan@hotmail.com>
2019-03-29 22:08:51 +0800

    Add CMA variant using local rank as fitness

--- SERVER_script.py
+++ SERVER_script.py
@@ -6,59 +6,22 @@ import config
 import SERVER_david_playerspace as david_playerspace
 import botlympics as botlympics
 from cma_player_space import CMAPlayerSpace
+from cma2_player_space import CMA2PlayerSpace
 from profile_player_space import ProfilePlayerSpace
 
 def createPlayerSpaces(taskmaster):
-    # Init player spaces here
-    #botlympics.init(taskmaster)
+    botlympics.init(taskmaster)
 
     david_playerspace.init(taskmaster, "2903_ChampPlat_Board")
 
-    CMAPlayerSpace(
+    CMA2PlayerSpace(
         taskmaster,
         'epsilon_player_train_apple',
         'EpsilonPlayer',
         [[-1, 1]] * 12,
-        4000,
         4,
         101,
         60 * 4)
-
-    ProfilePlayerSpace(
-        taskmaster,
-        'epsilon_player_profile_cherry',
-        'EpsilonPlayer',
-        10,
-        [[-1, 1]] * 12,
-        20,
-        4000,
-        1000,
-        4,
-        101,
-        60 * 4)
-
-    #CMAPlayerSpace(
-    #    taskmaster,
-    #    'neural3_player_train_cherry',
-    #    'Neural3Player',
-    #    [[-1, 1]] * 10 + [[0, 1]] * 2,
-    #    4000,
-    #    4,
-    #    101,
-    #    60 * 4)
-
-    #ProfilePlayerSpace(
-    #    taskmaster,
-    #    'neural3_player_profile_cherry',
-    #    'Neural3Player',
-    #    10,
-    #    [[-1, 1]] * 10 + [[0, 1]] * 2,
-    #    20,
-    #    4000,
-    #    1000,
-    #    4,
-    #    101,
-    #    60 * 4)
 
 class TrainerServer:
     def __init__(self, client_id=None):
