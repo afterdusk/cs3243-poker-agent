9cfbb383f17914e779d6d116df7969f67f87c9f8
LeeYiyuan <leeyiyuan@hotmail.com>
2019-04-17 11:49:44 +0800

    Enabel ProfilePlayerSpace for David's agent

--- SERVER_script.py
+++ SERVER_script.py
@@ -17,15 +17,15 @@ def createPlayerSpaces(taskmaster):
     # lj_playerspace.init(taskmaster, "070419_LJ_Training")
     david_playerspace.init(taskmaster, "1304_Lambda2_BoardIII")
 
-    # ProfilePlayerSpace(
-    #         taskmaster,
-    #         'LambdaPlayer',
-    #         'profile/0804_LambdaBoard History.txt',
-    #         [[-1, 1]] * 18,
-    #         10000,
-    #         4,
-    #         1000,
-    #         60 * 5)
+    ProfilePlayerSpace(
+            taskmaster,
+            'Lambda2Player',
+            '1304_Lambda2_BoardI History.txt',
+            [[-1, 1]] * 24,
+            10000,
+            4,
+            1000,
+            60 * 5)
 
     # CMA2PlayerSpace(
     #     taskmaster,
